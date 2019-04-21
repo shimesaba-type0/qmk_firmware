8a13fe4486de652ba54784d98b7ba246094e324e
shimesaba-type0 <t.k.shimesaba@gmail.com>
2019-04-21 23:08:20 +0900

    left hand

--- keyboards/ore_no_te/keymaps/default/keymap.c
+++ keyboards/ore_no_te/keymaps/default/keymap.c
@@ -23,8 +23,11 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
-    KC_A,  KC_1,  KC_H, \
-      KC_TAB,  KC_SPC   \
+       KC_ESC, KC_1, KC_2, KC_3,     KC_4,   KC_5,   \
+       KC_TAB, KC_Q, KC_W, KC_E,     KC_R,   KC_T,   \
+      KC_LCTL, KC_A, KC_S, KC_D,     KC_F,   KC_G,   KC_GRV, \
+      KC_LSFT, KC_Z, KC_X, KC_C,     KC_V,   KC_B,   KC_LBRC, \
+                  KC_LALT, KC_LCTL,  KC_SPC, KC_BSPC
   ),
 };
 
