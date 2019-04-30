18c17f8036b98706b5740e6befeaabcb7ab926a6
shimesaba-type0 <t.k.shimesaba@gmail.com>
2019-04-30 11:56:04 +0900

    serial communication test

--- keyboards/ore_no_te/config.h
+++ keyboards/ore_no_te/config.h
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DESCRIPTION     A custom keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 5
+#define MATRIX_ROWS 10
 #define MATRIX_COLS 7
 
 /*
@@ -51,13 +51,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
-#define SOFT_SERIAL_PIN D0 // or D1, D2, D3, E6
+#define SOFT_SERIAL_PIN D2 // or D1, D2, D3, E6
 
 // #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
 // #define BACKLIGHT_LEVELS 3
 
-// #define RGB_DI_PIN E2
+// #define RGB_DI_PIN D3
 // #ifdef RGB_DI_PIN
 //   #define RGBLED_NUM 16
 //   #define RGBLIGHT_HUE_STEP 8
--- keyboards/ore_no_te/keymaps/default/config.h
+++ keyboards/ore_no_te/keymaps/default/config.h
@@ -17,3 +17,5 @@
 #pragma once
 
 // place overrides here
+#define USE_SERIAL
+
