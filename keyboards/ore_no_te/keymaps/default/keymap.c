e1bdc455bbabc9f268809fc23a211511919ef12c
shimesaba-type0 <t.k.shimesaba@gmail.com>
2019-07-13 10:54:08 +0900

    Update: custom_keycode in LT() macro on ore_no_te

--- keyboards/ore_no_te/keymaps/default/keymap.c
+++ keyboards/ore_no_te/keymaps/default/keymap.c
@@ -55,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [_BASE] = LAYOUT( \
-     KC_ESC, KC_1, KC_2,              KC_3,             KC_4,           KC_5,                             KC_6,           KC_7,             KC_8,    KC_9,            KC_0,             KC_MINS, \
-     KC_TAB, KC_Q, KC_W,              KC_E,             KC_R,           KC_T,                             KC_Y,           KC_U,             KC_I,    KC_O,            KC_P,             KC_BSLS, \
-    KC_LCTL, KC_A, KC_S,              KC_D,             KC_F,           KC_G,           KC_GRV,  KC_EQL,  KC_H,           KC_J,             KC_K,    KC_L,            KC_SCLN,          KC_QUOT, \
-    KC_LSFT, KC_Z, KC_X,              KC_C,             KC_V,           KC_B,           KC_LBRC, KC_RBRC, KC_N,           KC_M,             KC_COMM, KC_DOT,          KC_SLSH,          KC_RSFT, \
-                   LT(META, KC_LALT), LT(SYMB, KC_ESC), LCTL_T(KC_SPC), SFT_T(KC_TAB),                    SFT_T(KC_BSPC), LT(META, KC_ENT), RCTL_T(KC_DELT), LT(SYMB, KC_RALT) \
+    KC_ESC,  KC_1, KC_2,               KC_3,              KC_4,           KC_5,                             KC_6,           KC_7,              KC_8,            KC_9,            KC_0,             KC_MINS, \
+    KC_TAB,  KC_Q, KC_W,               KC_E,              KC_R,           KC_T,                             KC_Y,           KC_U,              KC_I,            KC_O,            KC_P,             KC_BSLS, \
+    KC_LCTL, KC_A, KC_S,               KC_D,              KC_F,           KC_G,           KC_GRV,  KC_EQL,  KC_H,           KC_J,              KC_K,            KC_L,            KC_SCLN,          KC_QUOT, \
+    KC_LSFT, KC_Z, KC_X,               KC_C,              KC_V,           KC_B,           KC_LBRC, KC_RBRC, KC_N,           KC_M,              KC_COMM,         KC_DOT,          KC_SLSH,          KC_RSFT, \
+                   LT(_META, KC_LALT), LT(_SYMB, KC_ESC), LCTL_T(KC_SPC), SFT_T(KC_TAB),                    SFT_T(KC_BSPC), LT(_META, KC_ENT), RCTL_T(KC_DELT), LT(_SYMB, KC_RALT) \
   ),
 
 /* EUCALYN Custom
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `---------------------------'                 `---------------------------'
  */
  [_META] = LAYOUT( \
-     _______, XXXXXXX, XXXXXXX, ARROW,    ECLYN,   BASE,                          KC_HOME, KC_END,  KC_LCAP, KC_PSCR,  XXXXXXX, RESET, \
+     _______, XXXXXXX, XXXXXXX, ARROW,    ECLYN,   BASE,                         KC_HOME, KC_END,  KC_LCAP, KC_PSCR,  XXXXXXX, RESET, \
      _______, KC_1,    KC_2,    KC_3,     KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_PIPE, \
      _______, KC_F6,   XXXXXXX, KC_MHEN,  KC_HENK, XXXXXXX, KC_PLUS,  S(KC_MINS), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_F7,   XXXXXXX, \
      _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_ASTR,     KC_SLSH, KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_RSFT, \
