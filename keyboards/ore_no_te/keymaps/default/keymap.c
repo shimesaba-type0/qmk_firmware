339bce97d7a4ade4465821d57fbd75697a6690c2
shimesaba-type0 <t.k.shimesaba@gamil.com>
2019-08-12 14:31:45 +0900

    update: ore_no_te keymap

--- keyboards/ore_no_te/keymaps/default/keymap.c
+++ keyboards/ore_no_te/keymaps/default/keymap.c
@@ -38,6 +38,7 @@ enum custom_keycodes {
 };
 
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* BASE
  * ,-----------------------------------------.                 ,-----------------------------------------.
@@ -49,17 +50,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
  * | Sft  |   Z  |   X  |   C  |   V  |   B  |  [   |   |  ]   |   N  |   M  |   ,  |   .  |   /  |\/Sft |
  * `------+------+------+------+------+------+------'   `------+------+------+------+------+------+------|
- *               | META/|ESC/  |Space/|Tab/  |                 |BS/   |Enter/| Del/ | SYMB/|
- *               | LALT |~SYMB |LCtrl |Shift |                 |Shift |~META | RCTRL| RALT |
+ *               | META/|ESC/  |Space/|Tab/  |                 |BS/   |Enter/| SYMB/|RCTL/ |
+ *               | LALT |~SYMB |LCtrl |Shift |                 |Shift |~META |  DEL | RALT |
  *               `---------------------------'                 `---------------------------'
  */
 
   [_BASE] = LAYOUT( \
-    KC_ESC,  KC_1, KC_2,               KC_3,              KC_4,           KC_5,                             KC_6,           KC_7,              KC_8,            KC_9,            KC_0,             KC_MINS, \
-    KC_TAB,  KC_Q, KC_W,               KC_E,              KC_R,           KC_T,                             KC_Y,           KC_U,              KC_I,            KC_O,            KC_P,             KC_BSLS, \
-    KC_LCTL, KC_A, KC_S,               KC_D,              KC_F,           KC_G,           KC_GRV,  KC_EQL,  KC_H,           KC_J,              KC_K,            KC_L,            KC_SCLN,          KC_QUOT, \
-    KC_LSFT, KC_Z, KC_X,               KC_C,              KC_V,           KC_B,           KC_LBRC, KC_RBRC, KC_N,           KC_M,              KC_COMM,         KC_DOT,          KC_SLSH,          KC_RSFT, \
-                   LT(_META, KC_LALT), LT(_SYMB, KC_ESC), LCTL_T(KC_SPC), SFT_T(KC_TAB),                    SFT_T(KC_BSPC), LT(_META, KC_ENT), RCTL_T(KC_DELT), LT(_SYMB, KC_RALT) \
+    KC_ESC,  KC_1, KC_2,    KC_3,              KC_4,           KC_5,                             KC_6,           KC_7,              KC_8,              KC_9,            KC_0,    KC_MINS, \
+    KC_TAB,  KC_Q, KC_W,    KC_E,              KC_R,           KC_T,                             KC_Y,           KC_U,              KC_I,              KC_O,            KC_P,    KC_BSLS, \
+    KC_LCTL, KC_A, KC_S,    KC_D,              KC_F,           KC_G,           KC_GRV,  KC_EQL,  KC_H,           KC_J,              KC_K,              KC_L,            KC_SCLN, KC_QUOT, \
+    KC_LSFT, KC_Z, KC_X,    KC_C,              KC_V,           KC_B,           KC_LBRC, KC_RBRC, KC_N,           KC_M,              KC_COMM,           KC_DOT,          KC_SLSH, KC_RSFT, \
+                   MO(META),   LT(_SYMB, KC_ESC), LCTL_T(KC_SPC), SFT_T(KC_TAB),                    SFT_T(KC_BSPC), LT(_META, KC_ENT), LT(SYMB, KC_DEL), LT(KC_RCTL, KC_RALT) \
   ),
 
 /* EUCALYN Custom
@@ -73,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | LSFT |   Z  |  X   |   C  |   V  |   F  |  [   |   |  ]   |   B  |   G  |   T  |   S  |   /  | RSFT |
  * `------+------+------+------+------+------+------'   `------+------+------+------+------+------+------|
  *               | META/|ESC/  |Space/|Tab/  |                 |BS/   |Enter/| Del/ | SYMB/|
- *               | LALT |~SYMB |LCtrl |Shift |                 |Shift |~META | RCTRL| RALT |
+ *               |      |~SYMB |LCtrl |Shift |                 |Shift |~META | RCTRL| RALT |
  *               `---------------------------'                 `---------------------------'
  */
 
@@ -89,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* ARROW
  * ,-----------------------------------------.                 ,-----------------------------------------.
- * | ESC  |      |      |      |      |      |                 |      |      |      |      |      |Reset |
+ * | ESC  |      |      |      |      |      |                 |      |      |      |      |EEPRST|Reset |
  * |------+------+------+------+------+------.                 |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                 |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------.   ,-------------+------+------+------+------+------|
@@ -98,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |   |      |      |      |      |      |      |      |
  * `------+------+------+------+------+------+------'   `------+------+------+------+------+------+------|
  *               | META/|ESC/  |Space/|Tab/  |                 |BS/   |Enter/| Del/ | SYMB/|
- *               | LALT |~SYMB |LCtrl |Shift |                 |Shift |~META | RCTRL| RALT |
+ *               |      |~SYMB |LCtrl |Shift |                 |Shift |~META | RCTRL| RALT |
  *               `---------------------------'                 `---------------------------'
  */
 
@@ -114,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* META
  * ,-----------------------------------------.                 ,-----------------------------------------.
- * | Esc  |      |      |ARROW |ECLYN | BASE |                 | HOME |  END | CAPS |PrtSc |      |Reset |
+ * | Esc  |      |      |ARROW |ECLYN | BASE |                 | HOME |  END | CAPS |PrtSc |EEPRST|Reset |
  * |------+------+------+------+------+------.                 |------+------+------+------+------+------|
  * | Tab  |   1  |   2  |   3  |   4  |   5  |                 |   6  |   7  |   8  |   9  |   0  |  |   |
  * |------+------+------+------+------+------+------.   ,-------------+------+------+------+------+------|
@@ -123,20 +124,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Sft  |  F1  |  F2  |  F3  |  F4  |  F5  |  *   |   |  /   |  F8  |  F9  | F10  | F11  | F12  |\/Sft |
  * `------+------+------+------+------+------+------'   `------+------+------+------+------+------+------|
  *               | META/|ESC/  |Space/|Tab/  |                 |BS/   |Enter/| Del/ | SYMB/|
- *               | LALT |~SYMB |LCtrl |Shift |                 |Shift |~META | RCTRL| RALT |
+ *               |      |~SYMB |LCtrl |Shift |                 |Shift |~META | RCTRL| RALT |
  *               `---------------------------'                 `---------------------------'
  */
  [_META] = LAYOUT( \
-     _______, XXXXXXX, XXXXXXX, ARROW,    ECLYN,   BASE,                         KC_HOME, KC_END,  KC_LCAP, KC_PSCR,  XXXXXXX, RESET, \
+     _______, XXXXXXX, XXXXXXX, ARROW,    ECLYN,   BASE,                          KC_HOME, KC_END,  KC_LCAP, KC_PSCR,  EEP_RST, RESET, \
      _______, KC_1,    KC_2,    KC_3,     KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_PIPE, \
-     _______, KC_F6,   XXXXXXX, KC_MHEN,  KC_HENK, XXXXXXX, KC_PLUS,  S(KC_MINS), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_F7,   XXXXXXX, \
+     _______, KC_F6,   XXXXXXX, KC_MHEN,  KC_HENK, XXXXXXX, KC_PLUS,     KC_MINS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_F7,   XXXXXXX, \
      _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_ASTR,     KC_SLSH, KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_RSFT, \
                        _______, _______,  _______, _______,                       _______, _______, _______, _______ \
  ),
 
 /* SYMB
  * ,-----------------------------------------.                 ,-----------------------------------------.
- * | Esc  |      |      | CAPS |Prtsc |      |                 |      |      |      |      |      |Reset |
+ * | Esc  |      |      | CAPS |Prtsc |      |                 |      |      |      |      |EEPRST|Reset |
  * |------+------+------+------+------+------.                 |------+------+------+------+------+------|
  * | Tab  |   !  |   @  |   #  |   $  |   %  |                 |   ^  |   &  |   *  |   `  |   ~  |  |   |
  * |------+------+------+------+------+------+------.   ,-------------+------+------+------+------+------|
@@ -145,11 +146,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Sft  |      | APP  | GUI  |      |      |  [   |   |  ]   |   "  |   '  |   ;  | RALT |  ?   |\/Sft |
  * `------+------+------+------+------+------+------'   `------+------+------+------+------+------+------|
  *               | META/|ESC/  |Space/|Tab/  |                 |BS/   |Enter/| Del/ | SYMB/|
- *               | LALT |~SYMB |LCtrl |Shift |                 |Shift |~META | RCTRL| RALT |
+ *               |      |~SYMB |LCtrl |Shift |                 |Shift |~META | RCTRL| RALT |
  *               `---------------------------'                 `---------------------------'
  */
  [_SYMB] = LAYOUT( \
-     _______, XXXXXXX,  XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   RESET, \
+     _______, XXXXXXX,  XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EEP_RST,   RESET, \
      _______, KC_EXLM,  KC_AT,   KC_HASH,   KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_GRV,  S(KC_GRV), S(KC_BSLS), \
      _______, XXXXXXX,  KC_GRV,  S(KC_GRV), KC_LCBR, KC_LT,   KC_LPRN, KC_RPRN, KC_GT,   KC_RCBR, KC_COLN, KC_UNDS, KC_SLSH,   KC_JYEN, \
      _______, XXXXXXX,  KC_APP,  KC_LGUI,   XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_DQT,  KC_QUOT, KC_SCLN, KC_RALT, KC_QUES,   KC_RSFT, \
@@ -158,60 +159,62 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-void persistent_default_layer_set(uint16_t default_layer) {
+ void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
 }
 
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case BASE:
-      if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        SEND_STRING("This is BASE layer");
-        set_single_persistent_default_layer(1UL<<_BASE);
-      }
-      return false;
-      break;
-    case ECLYN:
-      if (record->event.pressed) {
-        // when keycode ARROW is pressed
-        SEND_STRING("Change layer to EUCALYN");
-        set_single_persistent_default_layer(1UL<<_ECLYN);
-      }
-      return false;
-      break;
-    case ARROW:
-      if (record->event.pressed) {
-        // when keycode ARROW is pressed
-        SEND_STRING("Change layer to ARROW");
-        set_single_persistent_default_layer(1UL<<_ARROW);
-      }
-      return false;
-      break;
-    case META:
-      if (record->event.pressed) {
-        // when keycode QMKURL is pressed
-        layer_on(_META);
-      } else {
-        // when keycode QMKURL is released
-        layer_off(_META);
-      }
-      return false;
-      break;
-    case SYMB:
-      if (record->event.pressed) {
-        // when keycode QMKURL is pressed
-        layer_on(_SYMB);
-      } else {
-        // when keycode QMKURL is released
-        layer_off(_SYMB);
-      }
-      return false;
-      break;
-  }
-  return true;
+ switch (keycode) {
+   case BASE:
+     if (record->event.pressed) {
+       // when keycode QMKBEST is pressed
+       // SEND_STRING("This is BASE layer");
+       set_single_persistent_default_layer(1UL<<_BASE);
+     }
+     return false;
+     break;
+   case ECLYN:
+     if (record->event.pressed) {
+       // when keycode ARROW is pressed
+       // SEND_STRING("Change layer to EUCALYN");
+       set_single_persistent_default_layer(1UL<<_ECLYN);
+     }
+     return false;
+     break;
+   case ARROW:
+     if (record->event.pressed) {
+       // when keycode ARROW is pressed
+       // SEND_STRING("Change layer to ARROW");
+       set_single_persistent_default_layer(1UL<<_ARROW);
+     }
+     return false;
+     break;
+   case META:
+     if (record->event.pressed) {
+       // when keycode QMKURL is pressed
+       layer_on(_META);
+     } else {
+       // when keycode QMKURL is released
+       layer_off(_META);
+     }
+     return false;
+     break;
+   case SYMB:
+     if (record->event.pressed) {
+       // when keycode QMKURL is pressed
+       layer_on(_SYMB);
+     } else {
+       // when keycode QMKURL is released
+       layer_off(_SYMB);
+     }
+     return false;
+     break;
+ }
+ return true;
 }
+
 
 void matrix_init_user(void) {
 
