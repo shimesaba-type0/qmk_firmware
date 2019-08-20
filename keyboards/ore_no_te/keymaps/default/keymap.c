69273ad88e2452d36e43498ed890991de8a69118
shimesaba-type0 <t.k.shimesaba@gamil.com>
2019-08-20 22:57:15 +0900

    update keymap

--- keyboards/ore_no_te/keymaps/default/keymap.c
+++ keyboards/ore_no_te/keymaps/default/keymap.c
@@ -50,17 +50,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
  * | Sft  |   Z  |   X  |   C  |   V  |   B  |  [   |   |  ]   |   N  |   M  |   ,  |   .  |   /  |\/Sft |
  * `------+------+------+------+------+------+------'   `------+------+------+------+------+------+------|
- *               | META/|ESC/  |Space/|Tab/  |                 |BS/   |Enter/| SYMB/|RCTL/ |
- *               |      |~SYMB |LCtrl |Shift |                 |Shift |~META |  DEL | RALT |
+ *               | G+S/ |C+S/  |Space/|Tab/  |                 |BS/   |Enter/| SYMB/|RCTL/ |
+ *               | META |~SYMB |LCtrl |Shift |                 |Shift |~META |  DEL | RALT |
  *               `---------------------------'                 `---------------------------'
  */
 
   [_BASE] = LAYOUT( \
-    KC_ESC,  KC_1, KC_2,    KC_3,              KC_4,           KC_5,                             KC_6,           KC_7,              KC_8,              KC_9,            KC_0,    KC_MINS, \
-    KC_TAB,  KC_Q, KC_W,    KC_E,              KC_R,           KC_T,                             KC_Y,           KC_U,              KC_I,              KC_O,            KC_P,    KC_BSLS, \
-    KC_LCTL, KC_A, KC_S,    KC_D,              KC_F,           KC_G,           KC_GRV,  KC_EQL,  KC_H,           KC_J,              KC_K,              KC_L,            KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z, KC_X,    KC_C,              KC_V,           KC_B,           KC_LBRC, KC_RBRC, KC_N,           KC_M,              KC_COMM,           KC_DOT,          KC_SLSH, KC_RSFT, \
-                   MO(_META),   LT(_SYMB, KC_ESC), LCTL_T(KC_SPC), SFT_T(KC_TAB),                    SFT_T(KC_BSPC), LT(_META, KC_ENT), LT(SYMB, KC_DEL), LT(KC_RCTL, KC_RALT) \
+    KC_ESC,  KC_1, KC_2, KC_3, KC_4, KC_5,                   KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, \
+    KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                   KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSLS, \
+    KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_GRV,      KC_EQL,  KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC,     KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
+    /* Left thumb keys */
+    LT(_META, G(KC_SPC)),   LT(_SYMB, C(KC_SPC)), LCTL_T(KC_SPC), SFT_T(KC_TAB), \
+                                                        /* Right thumb key */
+                                                        SFT_T(KC_BSPC), LT(_META, KC_ENT), LT(SYMB, KC_DEL), LT(KC_RCTL, KC_RALT) \
   ),
 
 /* EUCALYN Custom
@@ -119,19 +122,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------.                 |------+------+------+------+------+------|
  * | Tab  |   1  |   2  |   3  |   4  |   5  |                 |   6  |   7  |   8  |   9  |   0  |  |   |
  * |------+------+------+------+------+------+------.   ,-------------+------+------+------+------+------|
- * | Ctrl |  F6  |      |Muhen |Henk  |      |  +   |   |  -   | Left | Down | Up   |Right |  F7  |      |
+ * | Ctrl |      |      |Muhen |Henk  |      |  +   |   |  -   | Left | Down | Up   |Right |      |      |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
- * | Sft  |  F1  |  F2  |  F3  |  F4  |  F5  |  *   |   |  /   |  F8  |  F9  | F10  | F11  | F12  |\/Sft |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  *   |   |  /   |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * `------+------+------+------+------+------+------'   `------+------+------+------+------+------+------|
  *               | META/|ESC/  |Space/|Tab/  |                 |BS/   |Enter/| SYMB/|RCTL/ |
  *               |      |~SYMB |LCtrl |Shift |                 |Shift |~META |  DEL | RALT |
  *               `---------------------------'                 `---------------------------'
  */
  [_META] = LAYOUT( \
-     _______, XXXXXXX, XXXXXXX, ARROW,    ECLYN,   BASE,                          KC_HOME, KC_END,  KC_LCAP, KC_PSCR,  EEP_RST, RESET, \
+     _______, XXXXXXX, XXXXXXX, _ARROW,    _ECLYN,   BASE,                          KC_HOME, KC_END,  KC_LCAP, KC_PSCR,  EEP_RST, RESET, \
      _______, KC_1,    KC_2,    KC_3,     KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_PIPE, \
-     _______, KC_F6,   XXXXXXX, KC_MHEN,  KC_HENK, XXXXXXX, KC_PLUS,     KC_MINS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_F7,   XXXXXXX, \
-     _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_ASTR,     KC_SLSH, KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_RSFT, \
+     _______, XXXXXXX, XXXXXXX, KC_MHEN,  KC_HENK, XXXXXXX, KC_PLUS,     KC_MINS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, XXXXXXX, \
+     KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_ASTR,     KC_SLSH, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12, \
                        _______, _______,  _______, _______,                       _______, _______, _______, _______ \
  ),
 
@@ -141,20 +144,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------.                 |------+------+------+------+------+------|
  * | Tab  |   !  |   @  |   #  |   $  |   %  |                 |   ^  |   &  |   *  |   `  |   ~  |  |   |
  * |------+------+------+------+------+------+------.   ,-------------+------+------+------+------+------|
- * | Ctrl |      |   `  |   ~  |   {  |   <  |  (   |   |  )   |   >  |   }  |   :  |  _   |  /   | JPY  |
+ * | Ctrl |      |   `  |   ~  |      |      |  (   |   |  )   |   "  |   :  |   -  |  _   |  /   | JPY  |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
- * | Sft  |      | APP  | GUI  |      |      |  [   |   |  ]   |   "  |   '  |   ;  |  -   |  ?   |\/Sft |
+ * |      |      | APP  | GUI  |      |      |  [   |   |  ]   |      |   '  |      |      |  ?   |\/Sft |
  * `------+------+------+------+------+------+------'   `------+------+------+------+------+------+------|
  *               | META/|ESC/  |Space/|Tab/  |                 |BS/   |Enter/| SYMB/|RCTL/ |
  *               |      |~SYMB |LCtrl |Shift |                 |Shift |~META |  DEL | RALT |
  *               `---------------------------'                 `---------------------------'
  */
  [_SYMB] = LAYOUT( \
-     _______, XXXXXXX,  XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EEP_RST,   RESET, \
-     _______, KC_EXLM,  KC_AT,   KC_HASH,   KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_GRV,  S(KC_GRV), S(KC_BSLS), \
-     _______, XXXXXXX,  KC_GRV,  S(KC_GRV), KC_LCBR, KC_LT,   KC_LPRN, KC_RPRN, KC_GT,   KC_RCBR, KC_COLN, KC_UNDS, KC_SLSH,   KC_JYEN, \
-     _______, XXXXXXX,  KC_APP,  KC_LGUI,   XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_DQT,  KC_QUOT, KC_SCLN, KC_MINS, KC_QUES,   KC_RSFT, \
-                        _______, _______,   _______, _______,                   _______, _______, _______, _______ \
+     _______, XXXXXXX,  XXXXXXX, XXXXXXX,   XXXXXXX,   XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EEP_RST,   RESET, \
+     _______, KC_EXLM,  KC_AT,   KC_HASH,   KC_DLR,    KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_GRV,  S(KC_GRV), S(KC_BSLS), \
+     _______, XXXXXXX,  XXXXXXX, KC_GRV,    S(KC_GRV), XXXXXXX, KC_LPRN, KC_RPRN, KC_DQT,  KC_COLN, KC_MINS, KC_UNDS, KC_SLSH,   KC_JYEN, \
+     XXXXXXX, XXXXXXX,  KC_APP,  KC_LGUI,   XXXXXXX,   XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, KC_QUOT, XXXXXXX, XXXXXXX, KC_QUES,   KC_RSFT, \
+                        _______, _______,   _______,   _______,                   _______, _______, _______, _______ \
  ),
 
 };
