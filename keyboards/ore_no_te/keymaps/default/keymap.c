aad0a329261d1df189a80f7fe5b69ab4ed359352
shimesaba-type0 <t.k.shimesaba@gamil.com>
2019-09-09 16:43:16 +0900

    update ore_no_te default layout

--- keyboards/ore_no_te/keymaps/default/keymap.c
+++ keyboards/ore_no_te/keymaps/default/keymap.c
@@ -23,8 +23,8 @@ extern keymap_config_t keymap_config;
 
 enum layer_number {
     _BASE,
-    _ECLYN,     // Eucalyn Custom layout
-    _ARROW,     // Arrow
+    _ECLYN,
+    _ARROW,
     _META,
     _SYMB
 };
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------.                 |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                 |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------+------.   ,-------------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |  ~   |   |  =   |   H  |   J  |   K  |   L  |   ;  |  '   |
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |  ~   |   |  =   |   H  |   J  |   K  |   L  |   ;  |  -   |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
  * | Sft  |   Z  |   X  |   C  |   V  |   B  |  [   |   |  ]   |   N  |   M  |   ,  |   .  |   /  |\/Sft |
  * `------+------+------+------+------+------+------'   `------+------+------+------+------+------+------|
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT( \
     KC_ESC,  KC_1, KC_2, KC_3, KC_4, KC_5,                   KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, \
     KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                   KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSLS, \
-    KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_GRV,      KC_EQL,  KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
+    KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_GRV,      KC_EQL,  KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_MINS, \
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC,     KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
     /* Left thumb keys */
     LT(_META, G(KC_SPC)),   LT(_SYMB, C(KC_SPC)), LCTL_T(KC_SPC), SFT_T(KC_TAB), \
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------.                 |------+------+------+------+------+------|
  * | Tab  |   1  |   2  |   3  |   4  |   5  |                 |   6  |   7  |   8  |   9  |   0  |  |   |
  * |------+------+------+------+------+------+------.   ,-------------+------+------+------+------+------|
- * | Ctrl |      |      |Muhen |Henk  |      |  +   |   |  -   | Left | Down | Up   |Right |      |      |
+ * | Ctrl |      |      |Muhen |Henk  |      |  +   |   |  -   | Left | Down | Up   |Right |   .  |  -   |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  *   |   |  /   |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * `------+------+------+------+------+------+------'   `------+------+------+------+------+------+------|
@@ -131,16 +131,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `---------------------------'                 `---------------------------'
  */
  [_META] = LAYOUT( \
-     _______, XXXXXXX, XXXXXXX, _ARROW,    _ECLYN,   BASE,                          KC_HOME, KC_END,  KC_LCAP, KC_PSCR,  EEP_RST, RESET, \
+     _______, XXXXXXX, XXXXXXX, ARROW,    ECLYN,   BASE,                          KC_HOME, KC_END,  KC_LCAP, KC_PSCR,  EEP_RST, RESET, \
      _______, KC_1,    KC_2,    KC_3,     KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_PIPE, \
-     _______, XXXXXXX, XXXXXXX, KC_MHEN,  KC_HENK, XXXXXXX, KC_PLUS,     KC_MINS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, XXXXXXX, \
+     _______, XXXXXXX, XXXXXXX, KC_MHEN,  KC_HENK, XXXXXXX, KC_PLUS,     KC_MINS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_DOT,  _______, \
      KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_ASTR,     KC_SLSH, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12, \
                        _______, _______,  _______, _______,                       _______, _______, _______, _______ \
  ),
 
 /* SYMB
  * ,-----------------------------------------.                 ,-----------------------------------------.
- * | Esc  |      |      | CAPS |Prtsc |      |                 |      |      |      |      |EEPRST|Reset |
+ * | Esc  |      |      |      |      |      |                 |      |      |      |      |EEPRST|Reset |
  * |------+------+------+------+------+------.                 |------+------+------+------+------+------|
  * | Tab  |   !  |   @  |   #  |   $  |   %  |                 |   ^  |   &  |   *  |   `  |   ~  |  |   |
  * |------+------+------+------+------+------+------.   ,-------------+------+------+------+------+------|
